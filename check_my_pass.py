e80e092cc7b92eadf3cbb76d1730b8b9679a6002
nickrmeier <nickrmeier@gmaill.com>
2020-11-23 12:56:22 -0800

    api request made

--- check_my_pass.py
+++ check_my_pass.py
@@ -1,6 +1,20 @@
 import requests
+import hashlib
 
-url = 'https://api.pwnedpasswords.com/range/' + '5d414'
-res =  requests.get(url)
 
-print(res)
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    return sha1password
+
+
+pwned_api_check('hello')
