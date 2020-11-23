949e9d569b4bc2c80a30000558a960998fee1511
nickrmeier <nickrmeier@gmaill.com>
2020-11-23 13:34:30 -0800

    request to api working

--- check_my_pass.py
+++ check_my_pass.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -11,10 +12,33 @@ def request_api_data(query_char):
     return res
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
-    return sha1password
+    response = request_api_data(first5_char)
+    print(response)
+
+    return get_password_leaks_count(response, tail)
 
 
-pwned_api_check('hello')
+def main(args):
+    for password in args:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times, you should probably change it.')
+        else:
+            print(f'{password} was not found. Carry on!')
+    return 'done!'
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
